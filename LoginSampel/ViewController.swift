5322265c681651c5e562f35c4b6dde9183c845f3
Hakim Laoukili <hakimlaoukili@gmail.com>
2020-09-28 20:15:54 +0200

    Initial Commit

---
+++ LoginSampel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginSampel
+//
+//  Created by Hakim Laoukili on 2020-09-28.
+//  Copyright © 2020 Hakim Laoukili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoginSampelTests/LoginSampelTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoginSampelTests.swift
+//  LoginSampelTests
+//
+//  Created by Hakim Laoukili on 2020-09-28.
+//  Copyright © 2020 Hakim Laoukili. All rights reserved.
+//
+
+import XCTest
+@testable import LoginSampel
+
+class LoginSampelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
